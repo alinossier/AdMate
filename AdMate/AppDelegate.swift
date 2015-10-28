52be93690d24fdf26f595e103248f990e35597b5
anto0522 <antoninlinossier@gmail.com>
2015-10-28 18:38:13 -0400

    Initial Commit

---
+++ AdMate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdMate
+//
+//  Created by Antonin Linossier on 10/28/15.
+//  Copyright © 2015 Antonin Linossier. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AdMate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AdMate
+//
+//  Created by Antonin Linossier on 10/28/15.
+//  Copyright © 2015 Antonin Linossier. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
